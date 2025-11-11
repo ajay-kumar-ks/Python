f4bf1ebbaf8ae78f9e11ae62e2737cbc3b68b128
Ajaykumar ks <154059191+ajay-kumar-ks@users.noreply.github.com>
2025-11-11 11:18:45 +0530

    Add files via upload

---
+++ booksCls.py
@@ -0,0 +1,25 @@
+class Publisher :
+    def __init__(self,name):
+        self.name = name
+        
+class Book(Publisher):
+    def __init__(self, name,title,author):
+        super().__init__(name)
+        self.title = title
+        self.author = author
+        
+class Python(Book):
+    def __init__(self, name, title, author,price,no_of_pages):
+        super().__init__(name, title, author)
+        self.price = price
+        self.no_of_pages = no_of_pages
+        
+    def display(self):
+        print('Name => ',self.name)
+        print('title => ',self.title)
+        print('author => ',self.author)
+        print('price => ',self.price)
+        print('no_of_pages => ',self.no_of_pages)
+        
+r1 = Python('Nercotics','dirtyBusiness','ajay',1000,200)
+r1.display()
