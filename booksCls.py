0220bbd02f65b4c792e6d4982b9585aa9364b993
Ajaykumar ks <154059191+ajay-kumar-ks@users.noreply.github.com>
2025-11-11 11:36:08 +0530

    Update booksCls.py

--- booksCls.py
+++ booksCls.py
@@ -1,12 +1,18 @@
 class Publisher :
     def __init__(self,name):
         self.name = name
+    def display(self):
+        print('Name => ',self.name)
         
 class Book(Publisher):
     def __init__(self, name,title,author):
         super().__init__(name)
         self.title = title
         self.author = author
+    def display(self):
+        super().display()
+        print('title => ',self.title)
+        print('author => ',self.author)
         
 class Python(Book):
     def __init__(self, name, title, author,price,no_of_pages):
@@ -15,9 +21,7 @@ class Python(Book):
         self.no_of_pages = no_of_pages
         
     def display(self):
-        print('Name => ',self.name)
-        print('title => ',self.title)
-        print('author => ',self.author)
+        super().display()
         print('price => ',self.price)
         print('no_of_pages => ',self.no_of_pages)
         
